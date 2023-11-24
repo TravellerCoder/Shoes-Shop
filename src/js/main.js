a6d2673a629f420930f1a69f9a663abd2ed41ef5
travellercoder <guillermobukowski@gmail.com>
2023-11-23 22:51:28 -0300

    se crea array de productos

---
+++ src/js/main.js
@@ -0,0 +1,242 @@
+const productos = [
+    {
+        id: 'botin01',
+        titulo: 'Puma Borusia',
+        imagen: './images/botines/Borusia.webp',
+        categoria: {
+            id: 'botines',
+            nombre: 'Botines'
+        },
+        precio: 50000, 
+    },
+    {
+        id: 'botin02',
+        titulo: 'Puma Future Play',
+        imagen: './images/botines/FuturePlay.webp',
+        categoria: {
+            id: 'botines',
+            nombre: 'Botines'
+        },
+        precio: '70000', 
+    },
+    {
+        id: 'botin03',
+        titulo: 'Puma Future Z',
+        imagen: './images/botines/FutureZ.webp',
+        categoria: {
+            id: 'botines',
+            nombre: 'Botines'
+        },
+        precio: '55000', 
+    },
+    {
+        id: 'botin04',
+        titulo: 'Puma UltraMatch',
+        imagen: './images/botines/UltraMatch.webp',
+        categoria: {
+            id: 'botines',
+            nombre: 'Botines'
+        },
+        precio: '80000', 
+    },
+    {
+        id: 'botin05',
+        titulo: 'Adidas Copa',
+        imagen: './images/botines/Copa 19.3.webp',
+        categoria: {
+            id: 'botines',
+            nombre: 'Botines'
+        },
+        precio: '85000', 
+    },
+    {
+        id: 'botin06',
+        titulo: ' Adidas Predator Accuracy',
+        imagen: './images/botines/Predator Accuracy.jpg',
+        categoria: {
+            id: 'botines',
+            nombre: 'Botines'
+        },
+        precio: '100000', 
+    },
+    {
+        id: 'botin07',
+        titulo: 'Adidas Predato Mutator',
+        imagen: './images/botines/Predator Mutator.jpeg',
+        categoria: {
+            id: 'botines',
+            nombre: 'Botines'
+        },
+        precio: '95000', 
+    },
+    {
+        id: 'botin08',
+        titulo: 'Adidas X SpeedPortal',
+        imagen: './images/botines/X SpeedPortal.jpg',
+        categoria: {
+            id: 'botines',
+            nombre: 'Botines'
+        },
+        precio: '115000', 
+    },
+    {
+        id: 'botin09',
+        titulo: 'Nike Legend 8',
+        imagen: './images/botines/Legend8.webp',
+        categoria: {
+            id: 'botines',
+            nombre: 'Botines'
+        },
+        precio: '99000', 
+    },
+    {
+        id: 'botin10',
+        titulo: 'Nike Mercurial Superfly',
+        imagen: './images/botines/Mercurial Superfly.jpeg',
+        categoria: {
+            id: 'botines',
+            nombre: 'Botines'
+        },
+        precio: '105000', 
+    },
+    {
+        id: 'botin11',
+        titulo: 'Nike Phantom Vision',
+        imagen: './images/botines/Phantom Vision Pro.jpeg',
+        categoria: {
+            id: 'botines',
+            nombre: 'Botines'
+        },
+        precio: '100000', 
+    },
+    {
+        id: 'botin12',
+        titulo: 'Nike Zoom Vapor',
+        imagen: './images/botines/Zoom Vapor.jpg',
+        categoria: {
+            id: 'botines',
+            nombre: 'Botines'
+        },
+        precio: '120000', 
+    },
+    {
+        id: 'zapatilla01',
+        titulo: 'Nike Air Max 90',
+        imagen: './images/zapatillas/Air Max 90 Futura.webp',
+        categoria: {
+            id: 'zapatillas',
+            nombre: 'Zapatillas'
+        },
+        precio: '150000', 
+    },
+    {
+        id: 'zapatilla02',
+        titulo: 'Nike Air Max 97',
+        imagen: './images/zapatillas/Air Max 97 OG.jpeg',
+        categoria: {
+            id: 'zapatillas',
+            nombre: 'Zapatillas'
+        },
+        precio: '150000', 
+    },
+    {
+        id: 'zapatilla03',
+        titulo: 'Nike Renew Run',
+        imagen: './images/zapatillas/Renew Run3.jpeg',
+        categoria: {
+            id: 'zapatillas',
+            nombre: 'Zapatillas'
+        },
+        precio: '120000', 
+    },
+    {
+        id: 'zapatilla04',
+        titulo: 'Nike Air Force 1',
+        imagen: './images/zapatillas/Air Force 1 LE.jpg',
+        categoria: {
+            id: 'zapatillas',
+            nombre: 'Zapatillas'
+        },
+        precio: '150000', 
+    },
+    {
+        id: 'zapatiila05',
+        titulo: 'Adidas Force Bounce',
+        imagen: './images/zapatillas/ForceBounce 2.0.webp',
+        categoria: {
+            id: 'zapatillas',
+            nombre: 'Zapatillas'
+        },
+        precio: '135000', 
+    },
+    {
+        id: 'zapatilla06',
+        titulo: 'Adidas Forum Mid', 
+        imagen: './images/zapatillas/Forum Mid.jpg',
+        categoria: {
+            id: 'zapatillas',
+            nombre: 'Zapatillas'
+        },
+        precio: '148000', 
+    },
+    {
+        id: 'zapatilla07',
+        titulo: 'Adidas HVC',
+        imagen: './images/zapatillas/HVC.jpg',
+        categoria: {
+            id: 'zapatillas',
+            nombre: 'Zapatillas'
+        },
+        precio: '125000', 
+    },
+    {
+        id: 'zapatilla08',
+        titulo: 'Adidas Response Super',
+        imagen: './images/zapatillas/Response-Runner.webp',
+        categoria: {
+            id: 'zapatillas',
+            nombre: 'Zapatillas'
+        },
+        precio: '115000', 
+    },
+    {
+        id: 'zapatiila09',
+        titulo: 'Puma  R22',
+        imagen: './images/zapatillas/R22.jpeg',
+        categoria: {
+            id: 'zapatillas',
+            nombre: 'Zapatillas'
+        },
+        precio: '145000', 
+    },
+    {
+        id: 'zapatilla10',
+        titulo: 'Puma Rataliate',
+        imagen: './images/zapatillas/Rataliate2.jpeg',
+        categoria: {
+            id: 'zapatillas',
+            nombre: 'Zapatillas'
+        },
+        precio: '125000', 
+    },
+    {
+        id: 'zapatilla11',
+        titulo: 'Puma Suede Classic',
+        imagen: './images/zapatillas/Suede Classic.webp',
+        categoria: {
+            id: 'zapatillas',
+            nombre: 'Zapatillas'
+        },
+        precio: '100000', 
+    },
+    {
+        id: 'zapatilla12',
+        titulo: 'Puma Cali Dream',
+        imagen: './images/zapatillas/Cali Dream It.jpeg',
+        categoria: {
+            id: 'zapatillas',
+            nombre: 'Zapatillas'
+        },
+        precio: '125000', 
+    }
+]
